653e0aa9815d93f4c8b9eb4440de5dde624fee03
Jobson Almeida <jobsonbox@gmail.com>
2026-05-15 11:59:48 -0300

    accuracy table finished

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from pipeline.stage_30_motif_sequence.motif_sequence import run_motif_sequence
 from pipeline.stage_40_synchronization_matrix.synchronization_matrix import run_synchronization_matrix
 from pipeline.stage_60_synchronization_matrix_graph_measure.graph_measure import run_synchronization_matrix_graph_measure
 from pipeline.stage_80_lag_based_SVM.lag_based_SVM import run_lag_based_SVM
+from pipeline.stage_100_generate_results.generate_accuracy_table import run_generate_accuracy_table
+
 
 def main():
 
@@ -10,7 +12,9 @@ def main():
     #run_motif_sequence()
     #run_synchronization_matrix()
     #run_synchronization_matrix_graph_measure()
-    run_lag_based_SVM()
+    #run_lag_based_SVM()
+
+    run_generate_accuracy_table()
 
 
 if __name__ == "__main__":
--- pipeline/stage_100_generate_results/generate_accuracy_table.py
+++ pipeline/stage_100_generate_results/generate_accuracy_table.py
@@ -9,14 +9,19 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import StratifiedKFold, cross_val_score
 
+
 import pickle
+import pandas as pd
+import matplotlib.pyplot as plt
+
 
 
 current_file = Path(__file__).resolve()
 project_root = current_file.parents[2]
 
+result_name = "accuracy_table"
 base_path = project_root / "processed_data" / "stage_80_lag_based_SVM"
-output_path = project_root / "processed_data" / current_file.parents[0].name
+output_path = project_root / "results" / current_file.parents[0].name / result_name
 
 os.makedirs(output_path, exist_ok=True)
 
@@ -25,102 +30,175 @@ sessions = [f"ses-{i:02d}" for i in range(1, 4)]
 
 def run_generate_accuracy_table():
 
+    rows = []
+
     for subject in subjects:
 
-        graph_sessions = []
-        label_sessions = []
+        file_path = os.path.join(
+            base_path,
+            f"{subject}_svm_results.pkl"
+        )
 
-        for session in sessions:
+        if not os.path.exists(file_path):
+            print(f"Arquivo não encontrado: {file_path}")
+            continue
 
-            """___Abrindo o aquivo a ser processado___"""
+        with open(file_path, "rb") as f:
+            results = pickle.load(f)
 
-            file_path = os.path.join(
-                base_path,
-                f"{subject}_{session}_graph_measures.npy"
-            )
+        row = {
+            "Subject": subject
+        }
 
-            labels_path = os.path.join(
-                base_path,
-                f"{subject}_{session}_graph_measures_labels.npy"
-            )
+        for lag_name in results[subject]:
 
-            if not os.path.exists(file_path):
-                print(f"Arquivo não encontrado: {file_path}")
-                continue
+            mean_acc = results[subject][lag_name]["mean_accuracy"]
 
-            if not os.path.exists(labels_path):
-                print(f"Rótulos não encontrado: {labels_path}")
-                continue
+            row[lag_name] = mean_acc * 100
 
 
-            graph_measures = np.load(file_path)
-            labels = np.load(labels_path)
+        rows.append(row) 
 
-            graph_sessions.append(graph_measures)
-            label_sessions.append(labels)
 
-        # Unindo as 3 sessões do sujeito
-        subject_graph_measures = np.concatenate(graph_sessions, axis=0)
-        labels = np.concatenate(label_sessions, axis=0)
+    df = pd.DataFrame(rows) 
 
-        print(f"\n{subject} \nShape dos dados: {subject_graph_measures.shape} \nShape dos rótulos: {labels.shape}", )
+    # Anexando linhas de media e desvio padrao na tabela
+    mean_row = {
+        "Subject": "Mean"
+    }
 
-        results[subject] = {}
+    std_row = {
+        "Subject": "Std"
+    }
 
-        for lag_idx in range(subject_graph_measures.shape[2]):
+    lag_columns = [col for col in df.columns if col != "Subject"]
 
-            # Seleciona um lag específico
-            X = subject_graph_measures[:, :, lag_idx, :, :]
+    for col in lag_columns:
 
-            # Transforma em matriz 2D: (n_epochs, n_features)
-            X = X.reshape(X.shape[0], -1)
+        mean_row[col] = df[col].mean()
+        std_row[col] = df[col].std()
 
-            model = Pipeline([
-                ("scaler", StandardScaler()),
-                ("svm", SVC(
-                    kernel="rbf",
-                    C=1.0,
-                    gamma="scale",
-                    class_weight=None,
-                    tol=1e-3,
-                    max_iter=-1 # o treinamento 
-                ))
-            ])
+    df = pd.concat([
+        df,
+        pd.DataFrame([mean_row]),
+        pd.DataFrame([std_row])
+    ], ignore_index=True) 
 
-            cv = StratifiedKFold(
-                n_splits=5,
-                shuffle=True,
-                random_state=42
-            )
+    df[lag_columns] = df[lag_columns].round(2) # arredondando valores
 
-            scores = cross_val_score(
-                model,
-                X,
-                labels,
-                cv=cv,
-                scoring="accuracy"
-            )
+    desired_order = [
+        "Subject",
+        "lag_0",
+        "lag_1",
+        "lag_2",
+        "lag_3"
+    ]
 
-            results[subject][f"lag_{lag_idx}"] = {
-                "scores": scores,
-                "mean_accuracy": scores.mean(),
-                "std_accuracy": scores.std()
-            }
+    df = df[desired_order]
 
-            save_path = os.path.join(
-                output_path,
-                f"{subject}_lag_{lag_idx}_svm_results.pkl"
-            )
+    # Renomeando colunas
+    df = df.rename(columns={
+        "lag_0": "Lag 0 (%)",
+        "lag_1": "Lag 1 (%)",
+        "lag_2": "Lag 2 (%)",
+        "lag_3": "Lag 3 (%)"
+    })
 
-            with open(save_path, "wb") as f:
-                pickle.dump(results, f)
+    # renomeando linhas
+    df["Subject"] = df["Subject"].replace({
+        "sub-01": "S1",
+        "sub-02": "S2",
+        "sub-03": "S3",
+        "sub-04": "S4",
+        "sub-05": "S5",
+        "sub-06": "S6",
+        "sub-07": "S7",
+        "sub-08": "S8",
+        "sub-09": "S9",
+        "sub-10": "S10"
+    })
 
-            print(
-                subject,
-                f"lag_{lag_idx}",
-                "scores:", scores,
-                "mean:", scores.mean()
-            )
+    save_path = os.path.join(
+        output_path,
+        f"{result_name}.csv"
+    )
+
+    df.to_csv(save_path, index=False)
+
+
+    # Criar figura
+    fig, ax = plt.subplots(figsize=(10, 4))
+
+    # Remover eixos
+    ax.axis("off")
+
+    # Criar tabela
+    table = ax.table(
+        cellText=df.values,
+        colLabels=df.columns,
+        loc="center",
+        cellLoc="center"
+    )
+
+    # Ajustes visuais
+    table.auto_set_font_size(False)
+    table.set_fontsize(10)
+    table.scale(1.2, 1.5)
+
+
+    # Colocando maior valor de cada linha em negrito
+
+    # Colunas numéricas (ignorando Subject)
+    numeric_cols = df.columns[1:]
+
+    # Percorrendo linhas do dataframe
+    for row_idx in range(len(df)):
+
+        # Valores numéricos da linha
+        values = df.loc[row_idx, numeric_cols].astype(float)
+
+        # Índice da maior coluna
+        max_col_idx = values.argmax()
+
+        # Ajuste porque a coluna 0 é "Subject"
+        table_col_idx = max_col_idx + 1
+
+        # +1 porque a linha 0 da tabela é o cabeçalho
+        table_row_idx = row_idx + 1
+
+        # Aplicando negrito
+        table[(table_row_idx, table_col_idx)].set_text_props(
+            fontweight="bold"
+        )
+
+    # Título
+    ax.set_title(
+        "Table 1 - Mean SVM classification accuracy (%) obtained using 5-fold cross-validation",
+        fontsize=14,
+        fontweight="bold",
+        pad=20
+    )
+
+    plt.figtext(
+        0.05,
+        0.01,
+        "Values represent the mean classification accuracy (%) across 5-fold cross-validation.\n"
+        "For each lag condition, the training and test data were obtained using all filtered frequency bands.",
+        ha="left",
+        fontsize=10
+    )
+    
+    save_path = os.path.join(
+            output_path,
+            f"{result_name}.png"
+        )
+    # Salvar
+    plt.savefig(save_path, dpi=300, bbox_inches="tight")
+
+    plt.show()
+
+    
+
 
 
 if __name__ == "__main__":
