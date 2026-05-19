0d1c0749a573da6854917351bfea093bf80211e2
Jobson Almeida <jobsonbox@gmail.com>
2026-05-18 21:54:50 -0300

    previous merge

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ from pipeline.stage_100_generate_results.generate_confusion_matrix_for_maximum_m
 
 def main():
 
-    #run_band_filtering_and_epoch()
+    run_band_filtering_and_epoch()
     #run_motif_sequence()
     #run_synchronization_matrix()
-    run_maximum_matrix()
+    #run_maximum_matrix()
     
     #run_synchronization_matrix_graph_measure()
     #run_lag_based_SVM()
